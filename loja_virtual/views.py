aa9107a25bbf2bb9fec0f601ab615be569d556cf
romilsonlonan <romilsonlonan@gmail.com>
2024-05-26 09:54:23 -0300

    Criado loja_virtual/urls.py, loja_virtual/views.py

--- loja_virtual/views.py
+++ loja_virtual/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render 
 
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title": "Pagina principal"
+        "content" "Seja Bem a Pagina principal"
+    }
+    return render(request, "home_page.html", context)
