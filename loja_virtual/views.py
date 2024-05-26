3cf5eeacc56668acceac0421d7a425d1e07d851e
romilsonlonan <romilsonlonan@gmail.com>
2024-05-26 10:02:22 -0300

    Modificado gitignore e desenvolvendo as views

--- loja_virtual/views.py
+++ loja_virtual/views.py
@@ -6,4 +6,18 @@ def home_page(request):
         "title": "Pagina principal"
         "content" "Seja Bem a Pagina principal"
     }
-    return render(request, "home_page.html", context)
+    return render(request, "home_page.html", context) 
+
+def about_page(request):
+    context = {
+        "title": "Pagina sobre"
+        "content" "Seja Bem a Pagina sobre"
+    }
+    return render(request, "about_page.html", context)
+
+def contact_page(request):
+    context = {
+        "title": "Pagina de contatos"
+        "content" "Seja Bem a Pagina de contatos"
+    }
+    return render(request, "contact_page.html", context)
