5f7035ad653e46f3799cdc3d93f50fe802b5cdec
romilsonlonan <romilsonlonan@gmail.com>
2024-05-26 10:45:45 -0300

    Modificado urls ,views e template home page e adicionado as paginas sobre e contatos

--- loja_virtual/views.py
+++ loja_virtual/views.py
@@ -3,21 +3,21 @@ from django.shortcuts import render
 
 def home_page(request):
     context = {
-        "title": "Pagina principal"
-        "content" "Seja Bem a Pagina principal"
+        "title": "Pagina principal",
+        "content": "Seja Bem a Pagina principal"
     }
     return render(request, "home_page.html", context) 
 
 def about_page(request):
     context = {
-        "title": "Pagina sobre"
-        "content" "Seja Bem a Pagina sobre"
+        "title": "Pagina sobre",
+        "content": "Seja Bem a Pagina sobre"
     }
-    return render(request, "about_page.html", context)
+    return render(request, "about/view.html", context)
 
 def contact_page(request):
     context = {
-        "title": "Pagina de contatos"
-        "content" "Seja Bem a Pagina de contatos"
+        "title": "Pagina de contatos",
+        "content": "Seja Bem a Pagina de contatos"
     }
-    return render(request, "contact_page.html", context)
+    return render(request, "contact/view.html", context)
