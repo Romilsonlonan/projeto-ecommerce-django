e319b22904848c0006235f328ef786dbed01e96b
romilsonlonan <romilsonlonan@gmail.com>
2024-05-29 00:45:54 -0300

    Update products/models.py

--- products/models.py
+++ products/models.py
@@ -5,3 +5,6 @@ class Product(models.Model):
     description = models.TextField()
     price       = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
     
+    def __str__(self):
+        return self.title
+    
