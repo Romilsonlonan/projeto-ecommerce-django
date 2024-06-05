0651cbeeb61ab80bc7d4606b58bb06d859d33d7a
romilsonlonan <romilsonlonan@gmail.com>
2024-06-04 22:47:14 -0300

    Implementando classe ProductListView para exibir todos os produtos sem filtragem

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Product 
 
-# Create your views here.
+class ProductListView(ListView):
+    
+    # Traz todos os produtos do banco de dados sem filtrar nada 
+    queryset = Product.objects.all() 
+    
+# Função Based View    
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'qs': queryset
+    }
+    return render(request, "products/list.html", context)
