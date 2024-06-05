9334cd1310ac39976e034513c72126fe6085ab81
romilsonlonan <romilsonlonan@gmail.com>
2024-06-05 01:13:13 -0300

    Implementar product_detail_view para renderizar detalhes de um produto específico

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from typing import Any
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Product 
 
 class ProductListView(ListView):
@@ -21,3 +21,24 @@ def product_list_view(request):
         'qs': queryset
     }
     return render(request, "products/list.html", context)
+
+
+
+class product_detail_view(DetailView):
+    
+    # Traz todos os produtos do banco de dados sem filtrar nada 
+    queryset = Product.objects.all() 
+    template_name = "products/detail.html"
+    
+#    def get_context_data(self, *args, **kwargs):
+#        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+#        print (context)
+#        return context
+    
+# Função Based View    
+def product_detail_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'object_list': queryset
+    }
+    return render(request, "products/detail.html", context)
