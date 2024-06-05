b5b3dfd3f405c83a6a6ff0ddc78de5a3dcf90786
romilsonlonan <romilsonlonan@gmail.com>
2024-06-05 01:20:38 -0300

    Implementando as urls e os imports do detail_view

--- loja_virtual/urls.py
+++ loja_virtual/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from products.views import ProductListView, product_list_view
+from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -14,6 +14,8 @@ urlpatterns = [
     path('register/', register_page),
     path('products/', ProductListView.as_view()),
     path('products-fbv/', product_list_view),
+    path('products/', ProductDetailView.as_view()),
+    path('products-fbv/', product_detail_view),
 ]
 
 if settings.DEBUG: 
